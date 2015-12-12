24eb3a98304157ba95960e0345813a2437343b41
Jared Weinstock <jaredmarcweinstock@gmail.com>
2015-12-11 22:58:49 -0800

    hello website

---
+++ Gruntfile.js
@@ -0,0 +1,169 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        //--------------------
+        // clean :: remove directories/files
+        //--------------------
+
+        clean: {
+            main: 'dist'
+        },
+
+        //--------------------
+        // copy :: copies stuff
+        //--------------------
+
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'src',
+                src: [
+                    '*'
+                ],
+                dest: 'dist',
+                filter: 'isFile',
+                dot: true
+            }
+        },
+
+        //--------------------
+        // sass :: compile scss
+        //--------------------
+
+        sass: {
+            dev: {
+                options: { 
+                    outputStyle: 'expanded',
+                    sourceMap: true
+                },
+                files: {
+                    'src/styles/main.css': 'src/styles/main.scss'
+                }
+            },
+            dist: {
+                options: { 
+                    outputStyle: 'compressed',
+                    sourceMap: false
+                },
+                files: {
+                    'dist/styles/main-<%= pkg.version %>.css': 'src/styles/main.scss'
+                }
+            }
+        },
+
+        //--------------------
+        // imagemin :: optimize images
+        //--------------------
+
+        imagemin: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: [
+                        '**/*.*{png,jpg,gif}'
+                    ],
+                    dest: 'dist/images'
+                }]
+            }
+        },
+
+        //--------------------
+        // html-build :: dynamically build into html
+        //--------------------
+
+        htmlbuild: {
+            main: {
+                src: 'dist/index.html',
+                dest: 'dist',
+                options: {
+                    prefix: '/',
+                    styles: {
+                        main: {
+                            cwd: 'dist',
+                            files: 'styles/*.css'
+                        }
+                    }
+                }
+            }
+        },
+
+        //--------------------
+        // watch :: run tasks in response to file changes
+        //--------------------
+        
+        watch: {
+            options: {
+                interrupt: true
+            },
+            dev: { 
+                files: ['src/styles/**/*.scss'],
+                tasks: ['sass:dev']
+            },
+            dist: { 
+                files: ['src/styles/**/*.scss'],
+                tasks: ['sass:dist']
+            }
+        },
+
+        //--------------------
+        // connect :: start simple webserver
+        //--------------------
+
+        connect: {
+            dev: {
+                options: {
+                    base: 'src',
+                    port: 9000
+                }
+            },
+            dist: {
+                options: {
+                    base: 'dist',
+                    port: 9000
+                }
+            }
+        }
+    });
+
+    //-- load packages
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-html-build');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    //-- tasks
+
+    grunt.registerTask('default', [
+        'sass:dev',
+        'connect:dev',
+        'watch:dev'
+    ]);
+
+     grunt.registerTask('dist', [
+        'clean',
+        'copy',
+        'sass:dist',
+        'imagemin',
+        'htmlbuild',
+        'connect:dist',
+        'watch:dist'
+    ]);
+
+};
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# hello-website
+
+## Install
+
+- Install [node & npm](http://nodejs.org/download/)
+- `npm install -g grunt-cli` for [Grunt](http://gruntjs.com/getting-started)
+- `npm install -g bower` for [Bower](http://bower.io/)
+- `git clone https://github.com/wnstck/hello-website.git`
+- `cd hello-website/`
+- `npm install`
+- `bower install`
+
+## Run
+
+- `grunt`
+- <a href="http://localhost:9000">http://localhost:9000</a>
+
+## Build for Production
+
+- `grunt dist`
+- whatever else you need to do
